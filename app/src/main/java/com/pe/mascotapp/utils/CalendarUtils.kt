f55b63df71dec998ea3f95598bb8fb55b6dca5cd
joselipa <j.lipa.ochoa@gmail.com>
2024-10-13 22:39:01 -0500

    fix: in dates function

--- app/src/main/java/com/pe/mascotapp/utils/CalendarUtils.kt
+++ app/src/main/java/com/pe/mascotapp/utils/CalendarUtils.kt
@@ -210,7 +210,7 @@ fun LocalDate.inDates(
         return false
     }
     if (endDate == null){
-        return true
+        return (this.isAfter(startDate) || this.isEqual(startDate))
     }
     Log.e("quack",(this.isAfter(startDate) || this.isEqual(startDate)).toString())
     Log.e("quack",(this.isBefore(endDate) || this.isEqual(endDate)).toString())
